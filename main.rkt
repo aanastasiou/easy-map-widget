ce3b1a4d9b3a4e85de494c3c796d046a4530f72d
Athanasios Anastasiou <athanastasiou@gmail.com>
2026-01-13 18:30:15 +0100

    Added the option to initialise the widget with a specific class

--- main.rkt
+++ main.rkt
@@ -17,7 +17,12 @@
   
 (define easy-map-widget%
   (class* object% (view<%>)
-    (init-field @default-position @zoom-level @layers @center-on-layer @resize-to-fit-layer)
+    (init-field @default-position
+                @zoom-level
+                @layers
+                @center-on-layer
+                @resize-to-fit-layer
+                [map-widget-cl map-widget%])
     (super-new)
     ; Due to the way observables get updated, layer-names preserves
     ; a list of layer names that are already loaded to the map and it is
@@ -28,7 +33,7 @@
       (list @default-position @zoom-level @layers @center-on-layer @resize-to-fit-layer))
 
     (define/public (create parent)
-      (let [(widget (new map-widget%
+      (let [(widget (new map-widget-cl
                          [parent parent]
                          [position (obs-peek @default-position)]))]
         (define layers-value (obs-peek @layers))
@@ -74,21 +79,26 @@
                          (@zoom-level (obs 12))
                          (@layers (obs '()))
                          (@center-on-layer (obs #f))
-                         (@resize-to-fit-layer (obs #f)))
+                         (@resize-to-fit-layer (obs #f))
+                         #:use-widget-class [widget-class map-widget%])
   
   (new easy-map-widget%
        [@default-position @default-position]
        [@zoom-level @zoom-level]
        [@layers @layers]
        [@center-on-layer @center-on-layer]
-       [@resize-to-fit-layer @resize-to-fit-layer]))
+       [@resize-to-fit-layer @resize-to-fit-layer]
+       [map-widget-cl widget-class]))
 
 (define easy-map-widget-c (->* ()
                                ((obs/c (vectorof number?))
                                 (obs/c (integer-in 0 20))
                                 (obs/c (listof (is-a?/c layer<%>)))
                                 (obs/c (or/c symbol? #f))
-                                (obs/c (or/c symbol? #f)))
+                                (obs/c (or/c symbol? #f))
+                                #:use-widget-class (or/c #f
+                                                         (or/c (subclass?/c map-widget%)
+                                                               (subclass?/c map-snip%))))
                                (is-a?/c easy-map-widget%)))
                               
 (module+ test
@@ -102,6 +112,6 @@
   (test-case
    "is-in-list Item found in list"
    (check-eq? (is-in-list? '(1 2 3 4 5) 1) #t)
-  (test-case
-   "is-in-list Item does not exist in list"
-   (check-eq? (is-in-list? '(1 2 3 4 5) 42) #f))))
+   (test-case
+    "is-in-list Item does not exist in list"
+    (check-eq? (is-in-list? '(1 2 3 4 5) 42) #f))))
